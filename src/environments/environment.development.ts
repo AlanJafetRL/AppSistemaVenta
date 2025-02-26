f4408af7e636c0e72d53f18c7b3b4d084456b740
AlanJafetRL <alanjafet.rl@gmail.com>
2025-02-25 22:39:17 -0600

    Recursos e implementación de API REST

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    endpoint: "http://localhost:5196/api/"  //Uso general
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    endpoint: "http://localhost:5196/api/"  //Uso general
+};
