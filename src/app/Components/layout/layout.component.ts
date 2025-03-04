74474ae77c2329d6a2597a2d199afabbf9d17c9b
AlanJafetRL <alanjafet.rl@gmail.com>
2025-03-03 23:48:38 -0600

    Implementacion del menú dinámico, información de usuario y cerrar sesión

--- src/app/Components/layout/layout.component.ts
+++ src/app/Components/layout/layout.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { Menu } from '../../Interfaces/menu';
+
+import { MenuService } from '../../Services/menu.service';
+import { UtilidadService } from '../../Reutilizable/utilidad.service';
+
 
 @Component({
   selector: 'app-layout',
@@ -7,6 +14,36 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.scss',
   standalone:false
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  listaMenus:Menu[]=[];
+  correoUsuario:string='';
+  rolUsuario:string='';
+
+  constructor(
+    private router:Router,
+    private _menuServicio:MenuService,
+    private _utilidadServicio:UtilidadService
+  ){}
+
+  ngOnInit(): void {
+      const usuario=this._utilidadServicio.obtenerSesionUsuario();
+
+      if(usuario!= null){
+        this.correoUsuario=usuario.correo;
+        this.rolUsuario=usuario.rolDescripcion;
+
+        this._menuServicio.lista(usuario.idUsuario).subscribe({
+          next:(data)=>{
+            if(data.status) this.listaMenus=data.value;
+          },
+          error:(e)=>{}
+        });
+      }
+  }
+
+  cerrarSesion(){
+    this._utilidadServicio.eliminarSesionUsuario();
+    this.router.navigate(['login']);
+  }
 
 }
